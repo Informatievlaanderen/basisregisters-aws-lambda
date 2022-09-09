a891d964fd8e21650b135fca966769a3001787f1
jvandaal <jonas.vandaal@outlook.be>
2022-09-09 09:38:43 +0200

    test: extend function test

--- test/Be.Vlaanderen.Basisregisters.Aws.Lambda.Tests/TestFunction.cs
+++ test/Be.Vlaanderen.Basisregisters.Aws.Lambda.Tests/TestFunction.cs
@@ -6,15 +6,17 @@ namespace Be.Vlaanderen.Basisregisters.Aws.Lambda.Tests
     public class TestFunction : FunctionBase
     {
         public Action<string> MessageGroupIdAction { get; set; }
+        public Action<object?> MessageDataAction { get; set; }
 
-        public TestFunction(Action<string> messageGroupIdAction)
+        public TestFunction(Action<string> messageGroupIdAction, Action<object?> messageDataAction)
         {
             MessageGroupIdAction = messageGroupIdAction;
+            MessageDataAction = messageDataAction;
         }
 
         protected override IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IMessageHandler>(x => new TestMessageHandler(MessageGroupIdAction));
+            services.AddTransient<IMessageHandler>(x => new TestMessageHandler(MessageGroupIdAction, MessageDataAction));
 
             return services.BuildServiceProvider();
         }
