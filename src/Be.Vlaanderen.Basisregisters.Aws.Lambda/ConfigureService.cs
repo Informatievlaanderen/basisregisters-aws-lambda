df5b08924791b71118d9e15a61ed7838dcaaa4b5
Arne Dumarey <dumarey.arne@gmail.com>
2023-03-22 08:44:09 +0100

    refactor: refactor: move IConfiguration as parameter

--- src/Be.Vlaanderen.Basisregisters.Aws.Lambda/ConfigureService.cs
+++ src/Be.Vlaanderen.Basisregisters.Aws.Lambda/ConfigureService.cs
@@ -1,22 +1,16 @@
 namespace Be.Vlaanderen.Basisregisters.Aws.Lambda
 {
-    using System.IO;
     using Microsoft.Extensions.Configuration;
 
     public class ConfigureService : IConfigureService
     {
         public IEnvironmentService EnvironmentService { get; }
+        public IConfiguration Configuration { get; }
 
-        public ConfigureService(IEnvironmentService environmentService)
+        public ConfigureService(IEnvironmentService environmentService, IConfiguration configuration)
         {
+            Configuration = configuration;
             EnvironmentService = environmentService;
         }
-
-        public IConfiguration Configuration => new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{EnvironmentService.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
     }
 }
